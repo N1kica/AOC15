d4cec3df466eadb96586bac2e278af4c83a98367
N1kica <68340084+N1kica@users.noreply.github.com>
2024-01-09 11:03:56 +0100

    Update Day1.rs

--- Day1.rs
+++ Day1.rs
@@ -1,30 +1,25 @@
-use std::fs;
+fn part_one() -> i32 {
+    aoc::chars("./data/day16.txt", |c| Some(c))
+        .iter()
+        .fold(0, |acc, x| acc + step(x))
+}
 
-fn evaluate(c: &char) -> i32 {
-    match c {
+fn part_two() -> usize {
+    aoc::chars("./data/day16.txt", |c| Some(c))
+        .iter()
+        .scan(0, |acc, x| Some(std::mem::replace(acc, *acc + step(x))))
+        .position(|x| x == -1)
+        .unwrap()
+}
+
+fn step(inst: &char) -> i32 {
+    match inst {
         '(' => 1,
         _ => -1,
     }
 }
 
 fn main() {
-    let file = fs::read_to_string("input.txt").expect("Something went wrong reading the file");
-
-    let total_steps: i32 = file.chars().fold(0, |acc, c| {
-        acc + evaluate(&c)
-    });
-
-    println!("Santa takes {} total steps", total_steps);
-
-    let mut pos: i32 = 0;
-
-    let index = file.chars().enumerate().position(|(_i, c)| {
-        pos += evaluate(&c);
-        pos <= -1
-    });
-
-    match index {
-        Some(idx) => println!("Step when santa reaches basement: {}", &idx + 1),
-        None => println!("Santa doesn't reach the basement with given instructions"),
-    }
+    println!("Part 1: {}", part_one());
+    println!("Part 2: {}", part_two() + 1);
 }
